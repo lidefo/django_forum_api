f6d2b99ccdc1e38b894120e326b303a9e7deb2bd
lidefo <lidefo753@gmail.com>
2023-01-22 23:34:08 +0500

    now u can change DB_PORT and DB_HOST

--- config/settings.py
+++ config/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
